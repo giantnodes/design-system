1dd4f6d708ad603584323b3361da6ec0075238d3
Jordan Phillips <jordypee27@gmail.com>
2023-05-20 13:52:04 +1000

    feat(storybook): add configuration and setup using vite, tailwind and postcss

---
+++ .storybook/preview.tsx
@@ -0,0 +1,15 @@
+import type { Preview, StoryFn } from '@storybook/react'
+
+import './tailwind.css'
+
+export const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+}
