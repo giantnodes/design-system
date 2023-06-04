6eb2125e6e8fd86c1500dd22b02d7b29778f42f9
Jordan Phillips <jordypee27@gmail.com>
2023-06-04 10:30:42 +1000

    feat(avatar): add avatar notification and improve avatar group component

--- src/components/avatar/index.ts
+++ src/components/avatar/index.ts
@@ -2,5 +2,6 @@ export type { AvatarProps } from '@/components/avatar/Avatar'
 export type { AvatarGroupProps } from '@/components/avatar/AvatarGroup'
 export type { AvatarIconProps } from '@/components/avatar/AvatarIcon'
 export type { AvatarImageProps } from '@/components/avatar/AvatarImage'
+export type { AvatarNotificationProps } from '@/components/avatar/AvatarNotification'
 
 export { default as Avatar } from '@/components/avatar/Avatar'
