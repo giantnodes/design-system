7413d5db6cf6eefc1a4b8200d7d19d8b7412f5ec
Jordan Phillips <jordypee27@gmail.com>
2023-12-02 11:26:54 +1100

    fix: Component type using incorrect as type

--- packages/react/src/utilities/types.ts
+++ packages/react/src/utilities/types.ts
@@ -1,5 +1,8 @@
-export type Component<T extends React.ElementType> = React.ComponentPropsWithRef<T> & {
-  as?: T
+export type Component<
+  TElementType extends React.ElementType = 'div',
+  TProps = any,
+> = React.ComponentPropsWithRef<TElementType> & {
+  as?: React.ElementType<TProps>
 }
 
 export type ComponentWithoutAs<T extends React.ElementType> = Omit<Component<T>, 'as'>
