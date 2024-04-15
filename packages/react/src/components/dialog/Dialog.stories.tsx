5dcf058ee69dc1924802ea47ed3c0dd916036aff
Jordan Phillips <jordypee27@gmail.com>
2024-04-15 20:43:13 +1000

    feat(dialog): add dialog stories

---
+++ packages/react/src/components/dialog/Dialog.stories.tsx
@@ -0,0 +1,39 @@
+import type { DialogProps } from '@/components/dialog'
+import type { Meta, StoryFn } from '@storybook/react'
+
+import { Button } from '@/components/button'
+import { Card } from '@/components/card'
+import { Dialog } from '@/components/dialog'
+
+const Component: Meta<typeof Dialog> = {
+  title: 'Components/Dialog',
+  component: Dialog,
+}
+
+const defaultProps = {}
+
+export const Default: StoryFn = (args: DialogProps) => (
+  <Dialog {...args}>
+    <Button>Open</Button>
+
+    <Dialog.Content>
+      {({ close }) => (
+        <Card>
+          <Card.Header>Dialog</Card.Header>
+
+          <Card.Body>Dialog Content</Card.Body>
+
+          <Card.Footer>
+            <Button onPress={close}>Close</Button>
+          </Card.Footer>
+        </Card>
+      )}
+    </Dialog.Content>
+  </Dialog>
+)
+
+Default.args = {
+  ...defaultProps,
+}
+
+export default Component
