f40c8ebf7e57a1268441cc0dfd5017f920351fce
Jordan Phillips <jordypee27@gmail.com>
2024-04-16 17:06:41 +1000

    refactor: move useDomRef into hooks directory

--- packages/react/src/hooks/use-dom-ref.ts
+++ packages/react/src/hooks/use-dom-ref.ts
@@ -2,7 +2,6 @@ import type { Ref, RefObject } from 'react'
 
 import { useImperativeHandle, useRef } from 'react'
 
-// eslint-disable-next-line import/prefer-default-export
 export const useDomRef = <T extends HTMLElement = HTMLElement>(ref?: RefObject<T | null> | Ref<T | null>) => {
   const domRef = useRef<T>(null)
 
@@ -10,3 +9,5 @@ export const useDomRef = <T extends HTMLElement = HTMLElement>(ref?: RefObject<T
 
   return domRef
 }
+
+export type UseDomRefReturn = ReturnType<typeof useDomRef>
