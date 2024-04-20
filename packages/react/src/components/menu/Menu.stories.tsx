95f42ae6b8b2cffde964d7a2346f1b9fb5d5c720
Jordan Phillips <jordypee27@gmail.com>
2024-04-20 16:41:42 +1000

    fix: paragraph appearing as spans + className + typing issues (#24)
    * feat: add menu stories
    
    * fix: typescript + paragraph element type

---
+++ packages/react/src/components/menu/Menu.stories.tsx
@@ -0,0 +1,47 @@
+import type { MenuProps } from '@/components/menu'
+import type { Meta, StoryFn } from '@storybook/react'
+
+import { menu } from '@giantnodes/theme'
+
+import { Button } from '@/components/button'
+import { Menu } from '@/components/menu'
+
+const Component: Meta<typeof Menu> = {
+  title: 'Components/Menu',
+  component: Menu,
+  argTypes: {
+    size: {
+      control: { type: 'select' },
+    },
+    status: {
+      control: { type: 'select' },
+    },
+    variant: {
+      control: { type: 'select' },
+    },
+  },
+}
+
+const defaultProps = {
+  ...menu.defaultVariants,
+}
+
+export const Default: StoryFn = (args: MenuProps) => (
+  <Menu {...args}>
+    <Button>Open</Button>
+
+    <Menu.Popover placement="bottom right">
+      <Menu.List>
+        <Menu.Item>First</Menu.Item>
+        <Menu.Item>Second</Menu.Item>
+        <Menu.Item>Third</Menu.Item>
+      </Menu.List>
+    </Menu.Popover>
+  </Menu>
+)
+
+Default.args = {
+  ...defaultProps,
+}
+
+export default Component
