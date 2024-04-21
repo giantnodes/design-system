4240d352d7cf64e13d5a2abd2f4a3e6a696ea73a
Jordan Phillips <jordypee27@gmail.com>
2024-04-21 16:42:26 +1000

    feat(typography): add sizes to typography (#28)

--- packages/theme/src/components/typography.ts
+++ packages/theme/src/components/typography.ts
@@ -4,14 +4,28 @@ import { tv } from 'tailwind-variants'
 
 export const typography = tv({
   slots: {
-    paragraph: ['text-sm', 'antialiased', 'text-shark-400 dark:text-shark-100'],
+    paragraph: ['antialiased', 'text-shark-400 dark:text-shark-100'],
   },
   variants: {
+    size: {
+      sm: {
+        paragraph: ['text-sm'],
+      },
+      md: {
+        paragraph: ['text-md'],
+      },
+      lg: {
+        paragraph: ['text-lg'],
+      },
+    },
     variant: {
       subtitle: {
         paragraph: ['text-xs', 'text-shark-500 dark:text-shark-300'],
       },
     },
+  },
+  defaultVariants: {
+    size: 'sm',
   },
 })
 
