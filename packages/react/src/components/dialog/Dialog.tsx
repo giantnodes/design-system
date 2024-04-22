4b085612960832110051f2b0777d7264b78523a6
Jordan Phillips <jordypee27@gmail.com>
2024-04-22 18:13:35 +1000

    fix: dialog and menu components accepting refs that cannot be passed

--- packages/react/src/components/dialog/Dialog.tsx
+++ packages/react/src/components/dialog/Dialog.tsx
@@ -10,7 +10,7 @@ import { DialogContext, useDialog } from '@/components/dialog/use-dialog.hook'
 
 const __ELEMENT_TYPE__ = 'div'
 
-type ComponentOwnProps = Omit<DialogTriggerProps, 'children'> & DialogVariantProps
+type ComponentOwnProps = DialogTriggerProps & DialogVariantProps
 
 type ComponentProps<T extends React.ElementType> = Polymophic.ComponentPropsWithRef<T, ComponentOwnProps>
 
@@ -18,29 +18,27 @@ type ComponentType = <T extends React.ElementType = typeof __ELEMENT_TYPE__>(
   props: ComponentProps<T>
 ) => React.ReactNode
 
-const Component: ComponentType = React.forwardRef(
-  <T extends React.ElementType = typeof __ELEMENT_TYPE__>(props: ComponentProps<T>) => {
-    const { as, children, className, blur, placement, ...rest } = props
+const Component: ComponentType = <T extends React.ElementType = typeof __ELEMENT_TYPE__>(props: ComponentProps<T>) => {
+  const { as, children, className, blur, placement, ...rest } = props
 
-    const Element = as ?? DialogTrigger
+  const Element = as ?? DialogTrigger
 
-    const context = useDialog({ blur, placement })
+  const context = useDialog({ blur, placement })
 
-    const component = React.useMemo<Omit<DialogTriggerProps, 'children'>>(
-      () => ({
-        className: context.slots.dialog({ className }),
-        ...rest,
-      }),
-      [context.slots, className, rest]
-    )
+  const component = React.useMemo<Omit<DialogTriggerProps, 'children'>>(
+    () => ({
+      className: context.slots.dialog({ className }),
+      ...rest,
+    }),
+    [context.slots, className, rest]
+  )
 
-    return (
-      <DialogContext.Provider value={context}>
-        <Element {...component}>{children}</Element>
-      </DialogContext.Provider>
-    )
-  }
-)
+  return (
+    <DialogContext.Provider value={context}>
+      <Element {...component}>{children}</Element>
+    </DialogContext.Provider>
+  )
+}
 
 export type { ComponentOwnProps as DialogProps }
 export default Object.assign(Component, {
