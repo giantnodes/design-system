78e1c6c0d6910d7aea2f506b3585065b7f0ffc9f
Jordan Phillips <jordypee27@gmail.com>
2024-05-03 17:14:23 +1000

    refactor(table): use theme aware colours

--- packages/theme/src/components/table.ts
+++ packages/theme/src/components/table.ts
@@ -4,56 +4,37 @@ import { tv } from 'tailwind-variants'
 
 export const table = tv({
   slots: {
-    base: ['overflow-auto'],
-    table: ['table-auto', 'min-w-full', 'divide-shark-100 dark:divide-shark-500'],
+    table: ['bg-middleground', 'table-auto', 'min-w-full', 'overflow-hidden'],
     thead: [],
-    tbody: ['divide-y', 'divide-shark-100 dark:divide-shark-500'],
+    tbody: ['divide-y divide-partition'],
     tr: ['outline-none'],
-    th: [
-      'font-medium text-left',
-      'outline-none',
-      'whitespace-nowrap',
-      'text-shark-600 dark:text-shark-50',
-      'bg-white dark:bg-shark-950',
-      'first:rounded-l-md last:rounded-r-md',
-    ],
-    td: ['outline-none', 'whitespace-normal', 'text-shark-400 dark:text-shark-200'],
+    th: ['bg-background', 'text-title text-left font-medium', 'outline-none', 'whitespace-nowrap'],
+    td: ['text-content', 'whitespace-normal', 'outline-none'],
   },
   variants: {
     size: {
       sm: {
-        th: ['px-2', 'py-3', 'text-sm'],
-        td: ['px-2', 'py-2', 'text-sm'],
+        th: ['text-sm', 'px-2', 'py-3'],
+        td: ['text-sm', 'p-2'],
       },
       md: {
-        th: ['px-3', 'py-4', 'text-sm'],
-        td: ['px-3', 'py-3', 'text-sm'],
+        th: ['text-md', 'px-3', 'py-4'],
+        td: ['text-md', 'p-3'],
       },
       lg: {
-        th: ['px-4', 'py-5'],
-        td: ['px-4', 'py-4'],
+        th: ['text-lg', 'px-4 py-5'],
+        td: ['text-lg', 'p-4'],
       },
     },
     sticky: {
       true: {
-        table: ['border-separate', 'border-spacing-0'],
-        th: [
-          'sticky',
-          'top-0',
-          'bg-opacity-75',
-          'backdrop-blur',
-          'backdrop-filter',
-          'border-b',
-          'divide-shark-100 dark:divide-shark-500',
-          'z-10',
-        ],
-        td: ['border-b', 'border-shark-100 dark:border-shark-500'],
+        th: ['sticky top-0', 'bg-opacity-75', 'backdrop-blur backdrop-filter', 'border-b border-partition', 'z-10'],
       },
     },
     striped: {
       true: {
         tbody: ['divide-y-0'],
-        tr: ['odd:bg-transparent', 'even:bg-shark-100 even:dark:bg-shark-500'],
+        tr: ['odd:bg-foreground', 'even:bg-middleground'],
       },
     },
     headingless: {
