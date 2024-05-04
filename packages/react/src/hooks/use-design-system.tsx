ca92642b882b99616fd8f8f4772f69890fa52ed0
Jordan Phillips <jordypee27@gmail.com>
2024-05-04 13:59:03 +1000

    feat: add optional useHref prop to DesignSystemProvider

--- packages/react/src/hooks/use-design-system.tsx
+++ packages/react/src/hooks/use-design-system.tsx
@@ -2,9 +2,20 @@ import { RouterProvider } from '@react-aria/utils'
 
 export type UseDesignSystemProps = React.PropsWithChildren & {
   /**
-   * Provides a client side router to all components that contain links
+   * Function to perform client-side navigation.
+   *
+   * @param path - The path to navigate to.
+   * @returns void
    */
   navigate?: (path: string) => void
+
+  /**
+   * Optional function to convert a router specific href to a native HTML href.
+   *
+   * @param href - The router-specific href to convert.
+   * @returns The converted native HTML href.
+   */
+  useHref?: (href: string) => string
 }
 
 export const DesignSystemProvider: React.FC<UseDesignSystemProps> = ({ navigate, children }) => {
