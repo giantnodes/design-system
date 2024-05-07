10089f7cf2ac31ba2de27c8e68529da753afde79
Jordan Phillips <jordypee27@gmail.com>
2024-05-07 18:50:08 +1000

    feat(dialog): add size variant

--- packages/theme/src/components/dialog.ts
+++ packages/theme/src/components/dialog.ts
@@ -6,10 +6,21 @@ export const dialog = tv({
   slots: {
     dialog: [],
     overlay: ['flex', 'fixed inset-0 z-50', 'bg-black/50', 'min-h-full', 'overflow-y-auto', 'p-3'],
-    modal: ['w-full max-w-2xl', 'overflow-hidden'],
+    modal: ['w-full', 'overflow-hidden'],
     content: ['relative', 'outline-none'],
   },
   variants: {
+    size: {
+      sm: {
+        modal: ['max-w-2xl'],
+      },
+      md: {
+        modal: ['max-w-4xl'],
+      },
+      lg: {
+        modal: ['max-w-6xl'],
+      },
+    },
     blur: {
       true: {
         overlay: ['backdrop-blur-sm'],
@@ -26,6 +37,7 @@ export const dialog = tv({
     },
   },
   defaultVariants: {
+    size: 'md',
     blur: false,
     placement: 'center',
   },
