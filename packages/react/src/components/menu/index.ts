b9a966cc3a39c7cf65f88ae76b0fdb01548e86eb
Jordan Phillips <jordypee27@gmail.com>
2024-06-18 16:35:56 +1000

    feat: add menu trigger component

--- packages/react/src/components/menu/index.ts
+++ packages/react/src/components/menu/index.ts
@@ -2,5 +2,6 @@ export type * from '@/components/menu/Menu'
 export type * from '@/components/menu/MenuItem'
 export type * from '@/components/menu/MenuList'
 export type * from '@/components/menu/MenuPopover'
+export type * from '@/components/menu/MenuTrigger'
 
 export * as Menu from '@/components/menu/component.parts'
