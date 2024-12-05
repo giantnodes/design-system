12f9be4f8a04e5004c010468a78c9eb30bbbe9ce
Jordan Phillips <jordypee27@gmail.com>
2024-12-05 18:41:50 +1100

    chore: update react project dependencies

--- packages/react/src/hooks/use-design-system.tsx
+++ packages/react/src/hooks/use-design-system.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import type { ThemeProviderProps } from 'next-themes/dist/types'
+import type { ThemeProviderProps } from 'next-themes'
 import { ThemeProvider } from 'next-themes'
 import { RouterProvider } from 'react-aria-components'
 
