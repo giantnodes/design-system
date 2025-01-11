4f644199b232ee10152958024f598d997e2a94c4
Jordan Phillips <jordypee27@gmail.com>
2025-01-11 17:35:33 +1100

    fix: set storybook to use react-docgen-typescript

--- packages/react/.storybook/main.ts
+++ packages/react/.storybook/main.ts
@@ -12,6 +12,9 @@ const config: StorybookConfig = {
     name: '@storybook/react-vite',
     options: {},
   },
+  typescript: {
+    reactDocgen: 'react-docgen-typescript',
+  },
   docs: {
     autodocs: 'tag',
   },
