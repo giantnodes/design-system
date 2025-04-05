67dc65e51ce0e011dfd7b408467df7d95693518a
Jordan Phillips <jordypee27@gmail.com>
2025-04-05 18:36:41 +1100

    fix: storybook tailwindcss classes not building

--- packages/react/tailwind.config.ts
+++ packages/react/tailwind.config.ts
@@ -4,7 +4,7 @@ import { giantnodes } from '@giantnodes/theme'
 // https://javisperez.github.io/tailwindcolorshades
 
 const config: Config = {
-  content: ['./src/**/*.{ts,tsx}', './node_modules/@giantnodes/theme/dist/**/*.js'],
+  content: ['./src/**/*.{ts,tsx}', './node_modules/@giantnodes/theme/dist/**/*.{js,ts,jsx,tsx}'],
   plugins: [giantnodes()],
   darkMode: 'class',
 }
