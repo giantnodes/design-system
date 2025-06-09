9030879dc733e980c88c84ad75342d19b985a3b5
Jordan Phillips <jordypee27@gmail.com>
2025-06-09 11:05:26 +1000

    chore: upgrade dependencies (#367)
    * chore: upgrade dependencies
    
    * fix: tsconfig including temp files
    
    * chore: move files into src/ for consistent structure across packages
    
    * fix: eslint pointing to incorrect .gitignore path

--- tooling/eslint/src/react.js
+++ tooling/eslint/src/react.js
@@ -1,5 +1,4 @@
 import react from 'eslint-plugin-react'
-import compiler from 'eslint-plugin-react-compiler'
 import hooks from 'eslint-plugin-react-hooks'
 
 /** @type {Awaited<import('typescript-eslint').Config>} */
@@ -9,7 +8,6 @@ export default [
     plugins: {
       react: react,
       'react-hooks': hooks,
-      'react-compiler': compiler,
     },
     settings: {
       react: {
@@ -24,7 +22,7 @@ export default [
       'react/jsx-sort-props': ['error', { noSortAlphabetically: false, shorthandLast: true }],
       'react/function-component-definition': ['error', { namedComponents: 'arrow-function' }],
 
-      'react-compiler/react-compiler': 'error',
+      'react-hooks/react-compiler': 'error',
     },
     languageOptions: {
       globals: {
